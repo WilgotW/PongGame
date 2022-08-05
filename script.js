77038649a276d39836741f5c69abed504c1b3fcc
Wilgot <wilgotwardell@hotmail.com>
2022-08-05 16:32:17 +0200

    pong game complete

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ window.addEventListener('resize', function(){
 let keys = [];
 let walls = [];
 let players = [];
+let balls = [];
 class Wall {
     constructor(x, y, xLength, yLength){
         this.x = x;
@@ -41,17 +42,26 @@ class Ball {
         this.y = y;
         this.width = width;
         this.height = height;
+        this.xSpeed = 1;
+        this.ySpeed = 1;
+
+        this.xDir = 1;
+        this.yDir = 1;
     }
     draw(){
         c.fillStyle = "white";
         c.fillRect(this.x, this.y, this.width, this.height);
+    }
+    move(){
+        this.x += this.xSpeed * this.xDir;
+        this.y += this.ySpeed * this.yDir;
     }
 }
 
 
 // walls.push(new Wall(canvas.width* (1/8), canvas.height * (1/10), canvas.width * (6/8), 2));
 // walls.push(new Wall(canvas.width* (1/8), canvas.height * (5/10) , canvas.width * (6/8), 2));
-
+balls.push(new Ball(400, 300, 10, 10));
 walls.push(new Wall(100, 100, 600, 2));
 walls.push(new Wall(100, 500, 600, 2));
 walls.push(new Wall(100, 100, 2, 400));
@@ -68,10 +78,22 @@ function update(){
     players.forEach(player => {
         player.draw();
     });
+    balls.forEach(ball => {
+        ball.draw();
+        ball.move();
+    });
+    checkPlayerCollisions(balls[0], players);
+    checkWallCollisions(balls[0], walls);
+    
     keyboardInputs()
     requestAnimationFrame(update);
 }
 update();
+
+instantiateBall();
+function instantiateBall(){
+    
+}
 
 function refrech(){
     c.fillStyle = "black"
@@ -79,7 +101,7 @@ function refrech(){
 }
 
 document.addEventListener('keydown', function(event) {
-    console.log(event.keyCode);
+    // console.log(event.keyCode);
     keys[event.keyCode] = true;
 });
 document.addEventListener('keyup', function(event){
@@ -88,6 +110,7 @@ document.addEventListener('keyup', function(event){
 function keyboardInputs(){
     //w
     if(keys[87]){
+
         players[0].y -= 1
     }
     //s
@@ -101,4 +124,26 @@ function keyboardInputs(){
     if(keys[40]){
         players[1].y += 1
     }
+}
+
+function checkWallCollisions(ob, arr){
+    for (let i = 0; i < arr.length; i++) {
+        //x collision
+        if(ob.x + ob.width >= arr[i].x && ob.x <= arr[i].x){
+            ob.xDir *= -1;
+        }
+        //y collision
+        if(ob.y + ob.height >= arr[i].y && ob.y <= arr[i].y){
+            ob.yDir *= -1;
+        }
+    }   
+}
+function checkPlayerCollisions(ob, arr){
+    for (let i = 0; i < arr.length; i++) {        
+        //x collision
+        if(ob.x + ob.width >= arr[i].x && ob.x <= arr[i].x + arr[i].width && ob.y + ob.height >= arr[i].y && ob.y <= arr[i].y + arr[i].height){
+            //y collision
+            ob.xDir *= -1;
+        }
+    }   
 }
